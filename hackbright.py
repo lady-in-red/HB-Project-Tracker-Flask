ea098aa9d7c2e2eb19351b46a882bbdae9e57c5c
Hackbright Student <no-reply@hackbrightacademy.com>
2017-07-25 17:51:35 -0700

    WIP Student Creation From established

--- hackbright.py
+++ hackbright.py
@@ -47,7 +47,7 @@ def make_new_student(first_name, last_name, github):
     """
 
     QUERY = """
-        INSERT INTO Students
+        INSERT INTO Students (first_name, last_name, github)
           VALUES (:first_name, :last_name, :github)
         """
 
--- hackbright_web.py
+++ hackbright_web.py
@@ -28,6 +28,28 @@ def get_student_form():
 
     return render_template('student_search.html')
 
+@app.route("/student-creation")
+def student_creation_display():
+    """Shows the form that lets you add a new student."""
+
+    return render_template('make_student.html')
+
+
+@app.route('/student-add', methods=['POST'])
+def student_add():
+    """Returns a student added to database"""
+
+    github = request.form.get('github')
+    first = request.form.get('first_name')
+    last = request.form.get('last_name')
+
+    hackbright.make_new_student(first, last, github)
+
+    return render_template('new_student_added.html',
+                            github=github,
+                            first=first,
+                            last=last)
+
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
     app.run(debug=True)
