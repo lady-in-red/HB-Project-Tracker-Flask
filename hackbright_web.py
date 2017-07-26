38d17172e72ca3e1b2dea2281d13fa838e6f29af
Hackbright Student <no-reply@hackbrightacademy.com>
2017-07-25 18:04:13 -0700

    WIP Tracker Flask

--- hackbright_web.py
+++ hackbright_web.py
@@ -14,11 +14,14 @@ def get_student():
     github = request.args.get('github')
 
     first, last, github = hackbright.get_student_by_github(github)
+    project, grade = get_grades_by_github(github)
 
     html = render_template("student_info.html",
                            first=first,
                            last=last,
-                           github=github)
+                           github=github,
+                           project=project,
+                           grade=grade)
     return html
 
 
